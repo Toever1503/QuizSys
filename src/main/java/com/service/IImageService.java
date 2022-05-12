f5e63adc670c66bcb1e9013eb6ba18ece8757744
Toever1503 <Haunv1503@gmail.com>
2022-05-12 16:33:59 +0700

    Complete api upload

---
+++ src/main/java/com/service/IImageService.java
@@ -0,0 +1,8 @@
+package com.service;
+
+import com.entity.ImageEntity;
+import com.entity.model.ImageModel;
+import com.service.IBaseService;
+
+public interface IImageService extends IBaseService<ImageEntity, ImageModel, Long> {
+}
