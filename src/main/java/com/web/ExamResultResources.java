a967148fee9a9b3462e5103f07bfd8475427ac3b
Toever1503 <60539822+Toever1503@users.noreply.github.com>
2022-05-12 23:00:02 +0700

    update question save child (#13)

--- src/main/java/com/web/ExamResultResources.java
+++ src/main/java/com/web/ExamResultResources.java
@@ -4,6 +4,8 @@ import com.entity.dto.ExamResultDto;
 import com.entity.dto.ResponseDto;
 import com.entity.model.ExamResultModel;
 import com.service.IExamResultService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -21,22 +23,22 @@ public class ExamResultResources {
     }
     @Transactional
     @GetMapping
-    public Object getExamResults(Pageable page) {
+    public Object getExamResults(@ApiParam(example = "?page=0&size=10&sort=id,desc")Pageable page) {
         return ResponseDto.of(examResultService.findAll(page).map(ExamResultDto::toDto), "Get exam results");
     }
     @Transactional
     @GetMapping("{id}")
-    public Object getExamResult(@PathVariable Long id) {
+    public Object getExamResult(@ApiParam(name="Id of exam result") @PathVariable Long id) {
         return ResponseDto.of(ExamResultDto.toDto(examResultService.findById(id)), "Get exam result");
     }
     @Transactional
     @GetMapping("user/{id}")
-    public Object getExamResultsByUser(Long id, Pageable page) {
+    public Object getExamResultsByUser(@ApiParam(name="Id of user who has examed")Long id,@ApiParam(example = "?page=0&size=10&sort=id,desc")Pageable page) {
         return ResponseDto.of(examResultService.findAllByUser(id, page).map(ExamResultDto::toDto), "Get exam results by user");
     }
     @Transactional
     @GetMapping("subject/{id}")
-    public Object getExamResultsBySubject(Long id, Pageable page) {
+    public Object getExamResultsBySubject(@ApiParam(name="Id of subject")Long id, Pageable page) {
         return ResponseDto.of(examResultService.findAllBySubject(id, page).map(ExamResultDto::toDto), "Get exam results by subject");
     }
     @Transactional
@@ -48,18 +50,18 @@ public class ExamResultResources {
     }
     @Transactional
     @PatchMapping("{id}")
-    public Object updateExamResult(@PathVariable Long id, @RequestBody ExamResultModel examResultModel) {
+    public Object updateExamResult(@ApiParam(name="Id of exam result")@PathVariable Long id, @RequestBody ExamResultModel examResultModel) {
         examResultModel.setExamRsId(id);
         return ResponseDto.of(ExamResultDto.toDto(examResultService.update(examResultModel)), "Update exam result");
     }
     @Transactional
     @DeleteMapping("{id}")
-    public Object deleteExamResult(@PathVariable Long id) {
+    public Object deleteExamResult(@ApiParam(name="Id of exam result")@PathVariable Long id) {
         return ResponseDto.of(examResultService.deleteById(id), "Delete exam result");
     }
     @Transactional
     @DeleteMapping("batch/{ids}")
-    public Object deleteExamResults(@PathVariable("ids") List<Long> ids) {
+    public Object deleteExamResults(@ApiParam(name="array Id of exam result")@PathVariable("ids") List<Long> ids) {
         return ResponseDto.of(examResultService.deleteByIds(ids), "Delete exam results");
     }
 
