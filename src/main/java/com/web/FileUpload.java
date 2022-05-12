7cfd8d680fb07536f2d6114fae8955cc1a65ef5a
Toever1503 <Haunv1503@gmail.com>
2022-05-12 21:38:52 +0700

    Update file resources
    change image to files

--- src/main/java/com/web/FileUpload.java
+++ src/main/java/com/web/FileUpload.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/images")
+@RequestMapping("api/files")
 public class FileUpload {
 
     private final IImageService imageService;
