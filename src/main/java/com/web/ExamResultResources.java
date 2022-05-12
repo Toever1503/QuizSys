9e2b3ba3286a16a2ed0aa9892459f4bffa8b8b4e
Toever1503 <Haunv1503@gmail.com>
2022-05-12 13:56:52 +0700

    Complete  examResult curd

--- src/main/java/com/web/ExamResultResources.java
+++ src/main/java/com/web/ExamResultResources.java
@@ -1,16 +1,65 @@
 package com.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.entity.dto.ExamResultDto;
+import com.entity.dto.ResponseDto;
+import com.entity.model.ExamResultModel;
+import com.service.IExamResultService;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/examResults")
 public class ExamResultResources {
 
+    private final IExamResultService examResultService;
+
+    public ExamResultResources(IExamResultService examResultService) {
+        this.examResultService = examResultService;
+    }
+
     @GetMapping
-    public Object getExamResults() {
-        return "Exam results";
+    public Object getExamResults(Pageable page) {
+        return ResponseDto.of(examResultService.findAll(page).map(ExamResultDto::toDto), "Get exam results");
+    }
+
+    @GetMapping("{id}")
+    public Object getExamResult(@PathVariable Long id) {
+        return ResponseDto.of(ExamResultDto.toDto(examResultService.findById(id)), "Get exam result");
+    }
+
+    @GetMapping("user/{id}")
+    public Object getExamResultsByUser(Long id, Pageable page) {
+        return ResponseDto.of(examResultService.findAllByUser(id, page).map(ExamResultDto::toDto), "Get exam results by user");
+    }
+
+    @GetMapping("subject/{id}")
+    public Object getExamResultsBySubject(Long id, Pageable page) {
+        return ResponseDto.of(examResultService.findAllBySubject(id, page).map(ExamResultDto::toDto), "Get exam results by subject");
+    }
+
+    @PostMapping
+    @ResponseStatus(org.springframework.http.HttpStatus.CREATED)
+    public Object createExamResult(@RequestBody ExamResultModel examResultModel) {
+        examResultModel.setExamRsId(null);
+        return ResponseDto.of(ExamResultDto.toDto(examResultService.add(examResultModel)), "Create exam result");
+    }
+
+    @PatchMapping("{id}")
+    public Object updateExamResult(@PathVariable Long id, @RequestBody ExamResultModel examResultModel) {
+        examResultModel.setExamRsId(id);
+        return ResponseDto.of(ExamResultDto.toDto(examResultService.update(examResultModel)), "Update exam result");
+    }
+
+    @DeleteMapping("{id}")
+    public Object deleteExamResult(@PathVariable Long id) {
+        return ResponseDto.of(examResultService.deleteById(id), "Delete exam result");
+    }
+
+    @DeleteMapping("batch/{ids}")
+    public Object deleteExamResults(@PathVariable("ids") List<Long> ids) {
+        return ResponseDto.of(examResultService.deleteByIds(ids), "Delete exam results");
     }
 
 }
