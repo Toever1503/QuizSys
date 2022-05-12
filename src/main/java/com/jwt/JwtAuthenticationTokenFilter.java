c290bac30e4014cce766d2e9dd11cdab30399d4d
Toever1503 <Haunv1503@gmail.com>
2022-05-12 14:51:13 +0700

    Fix Confilct

--- src/main/java/com/jwt/JwtAuthenticationTokenFilter.java
+++ src/main/java/com/jwt/JwtAuthenticationTokenFilter.java
@@ -1,6 +1,9 @@
 package com.jwt;
 
 import com.repository.IUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 
@@ -10,6 +13,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
