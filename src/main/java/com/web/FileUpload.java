c6896498d9823b8f8a284d64813d865a5b18ef96
Toever1503 <Haunv1503@gmail.com>
2022-05-12 22:59:02 +0700

    update question save child

--- src/main/java/com/web/FileUpload.java
+++ src/main/java/com/web/FileUpload.java
@@ -4,6 +4,9 @@ import com.entity.ImageEntity;
 import com.entity.dto.ResponseDto;
 import com.entity.model.ImageModel;
 import com.service.IImageService;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.Example;
+import io.swagger.annotations.ExampleProperty;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +25,10 @@ public class FileUpload {
     public FileUpload(IImageService imageService) {
         this.imageService = imageService;
     }
+
     @Transactional
     @GetMapping
-    public Object getImages(Pageable page) {
+    public Object getImages(@ApiParam(example = "?page=0&size=10&sort=id,desc") Pageable page) {
         return ResponseDto.of(this.imageService.findAll(page), "Get all images");
     }
 
@@ -32,7 +36,7 @@ public class FileUpload {
     @Transactional
     @PostMapping
     @ResponseStatus(org.springframework.http.HttpStatus.CREATED)
-    public Object uploadFile(@RequestPart(name = "file") MultipartFile file) throws IOException {
+    public Object uploadFile(@ApiParam(name = "upload 1 file 1 time")@RequestPart(name = "file") MultipartFile file) throws IOException {
         if (file.isEmpty())
             throw new RuntimeException("Please select a file to upload");
         return imageService.add(ImageModel.builder().file(file).build());
@@ -41,7 +45,7 @@ public class FileUpload {
     @Transactional
     @PostMapping("batch")
     @ResponseStatus(org.springframework.http.HttpStatus.CREATED)
-    public Object uploadBatch(@RequestPart(name = "files") List<MultipartFile> files) {
+    public Object uploadBatch(@ApiParam(name = "upload batch file 1 time")@RequestPart(name = "files") List<MultipartFile> files) {
         if (files.get(0).isEmpty())
             throw new RuntimeException("Please select a file to upload");
         List<ImageEntity> imageEntities = new ArrayList<ImageEntity>();
@@ -54,21 +58,24 @@ public class FileUpload {
         }
         return ResponseDto.of(imageEntities, "Upload batch files");
     }
+
     @Transactional
     @PatchMapping("{id}")
-    public Object deleteFile(@PathVariable Long id, @RequestPart(name = "file") MultipartFile file) throws IOException {
+    public Object uploadFile(@ApiParam(name = "id of file", example = "1")@PathVariable Long id,@ApiParam(name = "new file to replace original file") @RequestPart(name = "file") MultipartFile file) throws IOException {
         if (file.isEmpty())
             throw new RuntimeException("Please select a file to upload");
         return imageService.update(ImageModel.builder().id(id).file(file).build());
     }
+
     @Transactional
     @DeleteMapping("{id}")
-    public Object deleteFile(@PathVariable Long id) {
+    public Object deleteFile(@ApiParam(name = "id of file", example = "1") @PathVariable Long id) {
         return ResponseDto.of(imageService.deleteById(id), "Delete file");
     }
+
     @Transactional
     @DeleteMapping("batch/{ids}")
-    public Object deleteBatch(@PathVariable List<Long> ids) {
+    public Object deleteBatch(@ApiParam(name = "array id of file", example = "1,2,3") @PathVariable List<Long> ids) {
         return ResponseDto.of(imageService.deleteByIds(ids), "Delete batch files");
     }
 }
